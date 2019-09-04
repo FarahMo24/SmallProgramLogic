a2c92e490414899f9253ad16336dc5801061284a
Mohamed Farah <47758699+FarahMo24@users.noreply.github.com>
2019-09-03 20:15:15 -0700

    Create Person.java

---
+++ AddToContacts/Person.java
@@ -0,0 +1,30 @@
+public class Person{
+
+  private String name;
+  private String number;
+  
+  public Person(String name, String number){
+    this.name = name;
+    this.number = number;
+  }
+  
+  public String getName(){
+    return this.name;
+  }
+  
+  public String getNumber(){
+    return this.number;
+  }
+  
+  public void changeNumber(String num){
+    this.number = num;
+  }
+  
+  public String toString(){
+    return this.name + " number: " + this.number;
+  }
+  
+    
+  
+  
+  
